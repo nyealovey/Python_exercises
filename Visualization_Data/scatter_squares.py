0b62192e4c7ba07dd4c1a73116106768e0e6b370
十忆九非 <shiyijiufei@gmail.com>
2020-11-26 15:32:47 +0800

    数据可视化练习

---
+++ Visualization_Data/scatter_squares.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, s=40)
+
+# 设置图表标题并给坐标轴加上标签
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value",fontsize=14)
+plt.axis([0, 1100, 0, 1100000])
+
+
+plt.show()
