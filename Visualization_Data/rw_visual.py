6352e17221c0dbf19e5fa798c86830ee4a0c6920
十忆九非 <shiyijiufei@gmail.com>
2020-11-26 16:10:31 +0800

    随机漫步

--- Visualization_Data/rw_visual.py
+++ Visualization_Data/rw_visual.py
@@ -5,8 +5,11 @@ from Visualization_Data.random_walk import RandomWalk
 # 创建一个Randomwalk的实例
 while True:
 
-    rw = RandomWalk(1000000)
+    rw = RandomWalk(100000)
     rw.fill_walk()
+    # 设置绘画窗口的尺寸
+    plt.figure(figsize=(10, 6))
+
     points_numbers = list(range(rw.num_points))
     plt.scatter(0,0, c='green',edgecolors='none',s=100)
     plt.scatter(rw.x_values[-1],rw.y_values[-1], c='red',
