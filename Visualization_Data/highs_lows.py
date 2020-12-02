c3175494017390fd150b49d58a612064803643bf
失忆就废 <shiyijiufei@gmail.com>
2020-12-02 08:42:03 +0800

    绘制温度完成

--- Visualization_Data/highs_lows.py
+++ Visualization_Data/highs_lows.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from datetime import datetime
 
 # 从文件中获取最高气温,最低和日期
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -12,12 +12,16 @@ with open(filename) as f:
     dates = []
     lows = []
     for row in reader:
-        high = int(row[1])
-        highs.append(high)
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-        low = int(row[3])
-        lows.append(low)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            highs.append(high)
+            dates.append(current_date)
+            lows.append(low)
     print(highs)
 
 # 根据数据绘制图形
